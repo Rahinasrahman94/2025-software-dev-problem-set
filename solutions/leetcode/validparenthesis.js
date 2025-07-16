a219422d0bf07e7806b87e3115e3016a6aefde59
rahinasrahman <rshinasrahman94@gmail.com>
2025-07-16 11:12:15 -0500

    validparenthesis.js

---
+++ solutions/leetcode/validparenthesis.js
@@ -0,0 +1,26 @@
+var isValid = function(s) {
+    let stack = [];
+    for (let c of s)
+    {
+       if(c === '(')
+       {
+        stack.push(')')
+       }
+       else if(c === '{')
+       {
+           stack.push('}')
+       } 
+       else if(c === '[')
+       {
+           stack.push(']')
+       }
+       
+       else if( stack.pop() !== c)
+       {
+        return false
+       }
+       
+       }
+       return stack.length === 0;
+        
+    }
