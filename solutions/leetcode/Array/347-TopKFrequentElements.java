c9501dddfc98c985fabd3e3932d60783d0580b57
rahinasrahman <rshinasrahman94@gmail.com>
2025-07-30 19:05:10 -0500

    problem-347

---
+++ solutions/leetcode/Array/347-TopKFrequentElements.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer , Integer>  m =  new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!m.containsKey(nums[i]))
+            {
+                m.put(nums[i] ,1);
+            }
+            else
+            {
+                m.put(nums[i],m.get(nums[i])+1);
+            }
+        }
+
+        List<Map.Entry<Integer,Integer>> entryList = new ArrayList<>(m.entrySet());
+        entryList.sort((a,b) -> b.getValue()-a.getValue());
+        int[] result= new int[k];
+        for(int i = 0;i<k;i++)
+        {
+            result[i] = entryList.get(i).getKey();
+        }
+        return result;
+    }
+
+}
